fe2944fdcd7bc9f00da3f09e312c2d01e9927fc4
Olga007 <o.piankova@ukr.net>
2024-02-26 10:28:03 +0200

    delete pages

--- src/App.js
+++ src/App.js
@@ -1,17 +1,8 @@
-import { Routes, Route } from 'react-router-dom';
-
-import { HomePage } from './pages/HomePage/HomePage';
-import { NotFoundPage } from './pages/NotFoundPage/NotFoundPage';
-
 import './App.css';
 
 function App() {
   return (
     <div>
-      <Routes>
-        <Route index element={<HomePage />} />
-        <Route path="*" element={<NotFoundPage />} />
-      </Routes>
     </div>
 
   )
--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
 
 import App from './App';
 
@@ -9,8 +8,6 @@ import './index.css';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename="trip">
-      <App />
-    </BrowserRouter>
+    <App />
   </React.StrictMode>
 );
