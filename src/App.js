5c0b5493f27729fc8a9fb43e6c20e857c85d523a
Olga007 <o.piankova@ukr.net>
2024-02-26 10:10:00 +0200

    added global styles, icons

--- src/App.js
+++ src/App.js
@@ -1,19 +1,22 @@
-import { Routes, Route } from 'react-router-dom';
+// import { Routes, Route } from 'react-router-dom';
+// import { HomePage } from './pages/HomePage/HomePage';
+// import { NotFoundPage } from './pages/NotFoundPage/NotFoundPage';
 
-import { HomePage } from './pages/HomePage/HomePage';
-import { NotFoundPage } from './pages/NotFoundPage/NotFoundPage';
+import { Container } from './components/Container/Container';
+import { Trip } from './components/Trip/Trip';
 
-import './App.css';
+import css from './App.module.css';
 
 function App() {
   return (
-    <div>
-      <Routes>
-        <Route index element={<HomePage />} />
-        <Route path="*" element={<NotFoundPage />} />
-      </Routes>
-    </div>
-
+    <Container>
+      <div className={css.wrapper}>
+        <h1>
+          Weather <span>Forecast</span>
+        </h1>
+        <Trip />
+      </div >
+    </Container>
   )
 };
 
